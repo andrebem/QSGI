8418042b281ac53f968e6159c80a3e4e2dbf8bd4
André Bem <profandrebem@gmail.com>
2015-10-14 17:59:25 -0300

    [ex1] Finishing Window class

--- Ex1/SGI2D/Window.cpp
+++ Ex1/SGI2D/Window.cpp
@@ -1,5 +1,5 @@
 #include "Window.h"
 
-Window::Window()
+Window::Window(const Ponto2D& min, const Ponto2D& max) : m_min(min), m_max(max)
 {
 }
--- Ex1/SGI2D/Window.h
+++ Ex1/SGI2D/Window.h
@@ -6,7 +6,27 @@
 class Window
 {
 public:
-    Window();
+    Window(const Ponto2D& min, const Ponto2D& max);
+
+    const Ponto2D& getMin() const
+    {
+        return m_min;
+    }
+
+    const Ponto2D& getMax() const
+    {
+        return m_max;
+    }
+
+    void setMin(const Ponto2D& min)
+    {
+        m_min = min;
+    }
+
+    void setMax(const Ponto2D& max)
+    {
+        m_max = max;
+    }
 
 private:
     Ponto2D m_min;
